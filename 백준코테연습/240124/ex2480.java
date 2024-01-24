7688440d29f5f9f4dbc18474b2a3d3743635b423
deq27 <155127920+deq27@users.noreply.github.com>
2024-01-24 23:24:52 +0900

    Create ex2480.java

---
+++ 백준코테연습/240124/ex2480.java
@@ -0,0 +1,33 @@
+package ex240124;
+
+import java.util.Scanner;
+
+public class ex2480 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if(a == b && b == c) {
+			System.out.println(10000 + a*1000);
+		}
+		else if(a == b || a == c) {
+			System.out.println(1000 + a*100);
+		}
+		else if(b == c) {
+			System.out.println(1000 + b*100);
+		}
+		else
+			System.out.println((Math.max(Math.max(a, b),c)*100));
+
+		sc.close();
+	}
+
+}
+
+
+
