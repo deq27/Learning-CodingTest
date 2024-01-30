0fc9f05841ac13cb7602e042a339d1b24eccdcd4
deq27 <155127920+deq27@users.noreply.github.com>
2024-01-30 22:31:11 +0900

    Create ex11382.java

---
+++ 백준코테연습/240130/ex11382.java
@@ -0,0 +1,22 @@
+package ex240130;
+
+import java.util.Scanner;
+
+public class ex11382 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		
+		long sum;
+		
+		System.out.println(sum=a+b+c);
+		
+		sc.close();
+	}
+
+}
